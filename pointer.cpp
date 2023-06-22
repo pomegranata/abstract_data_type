d06ac78a418c6b5f494617659750a3601e061b49
Pome <114458716+pomegranata@users.noreply.github.com>
2023-06-22 19:28:25 +0700

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+typedef struct info_sepeda{
+    char merk[10];
+    string type;
+    int tahun;
+    string harga;
+} spdh;
+
+int main()
+{
+    spdh info={0, 0, 0, 0};
+    spdh *pinfo;
+    pinfo=&info;
+
+    strcpy(pinfo->merk, "Polygon")
+    pinfo->type = "Sepeda Gunung";
+    pinfo->tahun = 2013;
+    pinfo->harga = "2.000.000";
+
+    cout << "Merk   : " << pinfo->merk << endl;
+    cout << "Type   : " << pinfo->type << endl;
+    cout << "Tahun  : " << pinfo->tahun << endl;
+    cout << "Harga  : " << pinfo->harga << endl;
+
+    return 0;
+}
